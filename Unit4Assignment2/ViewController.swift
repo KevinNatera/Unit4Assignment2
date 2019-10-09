37a56e3cd289101fe161a05c30ad55cf15812588
Kevin Natera <mycityneedsme@Kevins-MacBook-Pro-2.local>
2019-10-09 11:46:50 -0400

    InitialCommit

---
+++ Unit4Assignment2/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Unit4Assignment2
+//
+//  Created by Kevin Natera on 10/9/19.
+//  Copyright © 2019 Kevin Natera. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
